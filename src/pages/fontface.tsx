3b1f7d35fd96e551cec73c69d110b5da1824aaf9
Vishal <donvip56@gmail.com>
2025-08-01 14:16:00 +0530

    g

--- src/pages/fontface.tsx
+++
@@ -1,8 +0,0 @@
-import { useFonts } from 'expo-font';
-
- export default function useSatoshiFont  ()  {
- const [fontsLoaded] = useFonts({
-  'Satoshi': require('../assets/fonts/Satoshi.ttf'),
- });
- return fontsLoaded;
-};
